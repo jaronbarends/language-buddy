d934d500f63ca9fc2dd6f2f223296b59a9c7d902
jaronbarends <731645+jaronbarends@users.noreply.github.com>
2026-08-21 13:01:41 +0200

    add logo with css

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,15 @@
+import Icon from './icon/Icon';
+
 import styles from './Logo.module.css';
 
 export default function Logo() {
-  return <div className={styles.logo}> </div>;
+  // return <div className={styles.logoOld}> </div>;
+  return (
+    <div className={styles.logo}>
+      <div className={styles.balloon}>
+        <Icon iconName="evaluation" />
+      </div>
+      <div className={styles.arrow} />
+    </div>
+  );
 }
--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -1,11 +1,14 @@
 import { type ReactNode } from 'react';
 
+import Logo from './Logo';
+
 import styles from './PageHeading.module.css';
 
 export default function PageHeading({ children }: { children: ReactNode }) {
   return (
     <div className={styles.pageHeading}>
-      <div className={styles.logo}></div>
+      {/* <div className={styles.logo}></div> */}
+      <Logo />
       <div>{children}</div>
     </div>
   );
